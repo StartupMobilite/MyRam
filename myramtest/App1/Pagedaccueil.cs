b8a956643950c53a8785f37bdf95c988a584251c
xXfergalXx <fergal.bougerol@ynov.com>
2016-01-13 16:19:13 +0100

    insertion du projet

---
+++ myramtest/App1/Pagedaccueil.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic; 
+ using Android.App; 
+ using Android.Content; 
+ using Android.OS; 
+ using Android.Widget; 
+ using Tasky.Shared; 
+ using TaskyAndroid; 
+ using TaskyAndroid.ApplicationLayer; 
+ using Android.Content.PM; 
+ 
+ 
+ namespace TaskyAndroid.Screens
+ { 
+ 	
+ 	[Activity (Label = "Tasky",   
+ 		Icon="@drawable/icon", 
+ 		MainLauncher = true, 
+ 		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, 
+ 		ScreenOrientation = ScreenOrientation.Portrait)] 
+ 	public class HomeScreen : Activity  
+ 	{ 
+ 		TodoItemListAdapter taskList; 
+ 		IList<TodoItem> tasks; 
+ 		Button addTaskButton; 
+ 		ListView taskListView; 
+ 		 
+ 		protected override void OnCreate(Bundle bundle)
+ 		{ 
+ 			base.OnCreate(bundle); 
+ 
+ 
+ 			
+ 			SetContentView(Resource.Layout.HomeScreen); 
+ 
+ 
+ 		
+ 			taskListView = FindViewById<ListView> (Resource.Id.TaskList); 
+ 			addTaskButton = FindViewById<Button> (Resource.Id.AddButton); 
+ 
+ 
+ 			 
+ 			if(addTaskButton != null) { 
+ 				addTaskButton.Click += (sender, e) => { 
+ 					StartActivity(typeof(TodoItemScreen)); 
+ 				}; 
+ 			} 
+ 			 
+ 			
+ 			if(taskListView != null) { 
+ 				taskListView.ItemClick += (object sender, AdapterView.ItemClickEventArgs e) => { 
+ 					var taskDetails = new Intent(this, typeof(TodoItemScreen)); 
+ 					taskDetails.PutExtra ("TaskID", tasks[e.Position].ID); 
+ 					StartActivity(taskDetails); 
+ 				}; 
+ 			} 
+ 		} 
+ 		 
+ 		protected override void OnResume()
+ 		{ 
+ 			base.OnResume(); 
+ 
+ 
+ 			tasks = TodoItemManager.GetTasks(); 
+ 			 
+ 			
+ 			taskList = new TodoItemListAdapter(this, tasks); 
+ 
+ 
+ 			
+ 			taskListView.Adapter = taskList; 
+ 		} 
+ 	} 
+ } 
---
+++ myramtest/Class1.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic; 
+using Android.App;
+using Android.Widget; 
+using Tasky.Shared;  
+ 
+namespace TaskyAndroid.ApplicationLayer
+ { 
+ 	
+	public class TodoItemListAdapter : BaseAdapter<TodoItem>  
+ 	{ 
+ 		Activity context = null; 
+ 		IList<TodoItem> tasks = new List<TodoItem>(); 
+ 		 
+ 		public TodoItemListAdapter(Activity context, IList<TodoItem> tasks) : base()
+ 		{ 
+ 			this.context = context; 
+ 			this.tasks = tasks; 
+ 		} 
+ 		 
+ 		public override TodoItem this[int position]
+ 		{
+              get { return tasks[position]; }
+          } 
+	 
+ 		public override long GetItemId (int position) 
+ 		{
+              return position;
+          } 
+	 
+ 		public override int Count 
+ 		{
+              get { return tasks.Count; }
+          } 
+	 
+ 		public override Android.Views.View GetView (int position, Android.Views.View convertView, Android.Views.ViewGroup parent) 
+ 		{
+     			 
+        var item = tasks[position];
+    
+
+		
+          var view = (convertView ??
+              context.LayoutInflater.Inflate(
+                  Android.Resource.Layout.SimpleListItemChecked,
+                  parent,
+                  false)) as CheckedTextView;
+              view.SetText(item.Name == "" ? "<new task>" : item.Name, TextView.BufferType.Normal);
+              view.Checked = item.Done;
+    
+
+
+
+ 			
+          return view;
+          }
+ 	} 
+ }
+
